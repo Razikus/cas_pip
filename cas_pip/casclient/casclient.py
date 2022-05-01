a57b33f05e6bd011c6799dec1c3ec171573488fe
Adam Raźniewski <adam@razniewski.eu>
2022-05-01 19:14:23 +0200

    update

--- cas_pip/casclient/casclient.py
+++ cas_pip/casclient/casclient.py
@@ -2,8 +2,11 @@ from enum import IntEnum
 import asyncio
 import logging
 from typing import List
+from pip import __version__ as pipVersion
 from pip._internal.cli.main import main as _main
 import json
+import hashlib
+from typing import Union
 
 class NotarizedStatus(IntEnum):
     NOTARIZED = 0
@@ -28,17 +31,82 @@ class CASClient:
         await proc.wait()
         stdout, stderr = await proc.communicate()
         return stdout, stderr
+
+    def getSha256(self, fromWhat: Union[str, bytes], strEncoding = "utf-8"):
+        hashed = hashlib.sha256()
+        if(type(fromWhat) == str):
+            hashed.update(fromWhat.encode(strEncoding))
+        else:
+            hashed.update(fromWhat)
+        return hashed.hexdigest()
+
+    async def notarizeHash(self, hash, packageName):
+        cmd = [self.casPath, "notarize", "--hash", hash, "--output=json", "--api-key", self.apiKey, "--name", packageName]
+        stdout, stderr = await self.executeMe(cmd)
+        if(stderr):
+            self.logger.error(packageName + " " +  stderr.decode("utf-8"))
+        return packageName, json.loads(stdout)
     
     async def notarizeFile(self, absolutePath, packageName):
-        cmd = [self.casPath, "notarize", absolutePath, "--output=json", "--api-key", self.apiKey]
+        cmd = [self.casPath, "notarize", absolutePath, "--output=json", "--api-key", self.apiKey, "--name", packageName]
         stdout, stderr = await self.executeMe(cmd)
         if(stderr):
             self.logger.error(packageName + " " +  stderr.decode("utf-8"))
         return packageName, json.loads(stdout)
 
+    async def notarizeHash(self, hash, packageName):
+        cmd = [self.casPath, "notarize", "--hash", hash, "--output=json", "--api-key", self.apiKey, "--name", packageName]
+        stdout, stderr = await self.executeMe(cmd)
+        if(stderr):
+            self.logger.error(packageName + " " +  stderr.decode("utf-8"))
+        return packageName, json.loads(stdout)
+    
+    async def unsupportFile(self, absolutePath, packageName):
+        cmd = [self.casPath, "unsupport", absolutePath, "--output=json", "--api-key", self.apiKey, "--name", packageName]
+        stdout, stderr = await self.executeMe(cmd)
+        if(stderr):
+            self.logger.error(packageName + " " +  stderr.decode("utf-8"))
+        return packageName, json.loads(stdout)
 
-    async def authenticateFile(self, packageName, package):
-        cmd = [self.casPath, "authenticate", package, "--output=json"]
+    async def unsupportHash(self, hash, packageName):
+        cmd = [self.casPath, "unsupport", "--hash", hash, "--output=json", "--api-key", self.apiKey, "--name", packageName]
+        stdout, stderr = await self.executeMe(cmd)
+        if(stderr):
+            self.logger.error(packageName + " " +  stderr.decode("utf-8"))
+        return packageName, json.loads(stdout)
+    
+    async def untrustFile(self, absolutePath, packageName):
+        cmd = [self.casPath, "untrust", absolutePath, "--output=json", "--api-key", self.apiKey, "--name", packageName]
+        stdout, stderr = await self.executeMe(cmd)
+        if(stderr):
+            self.logger.error(packageName + " " +  stderr.decode("utf-8"))
+        return packageName, json.loads(stdout)
+
+    async def untrustHash(self, hash, packageName):
+        cmd = [self.casPath, "untrust", "--hash", hash, "--output=json", "--api-key", self.apiKey, "--name", packageName]
+        stdout, stderr = await self.executeMe(cmd)
+        if(stderr):
+            self.logger.error(packageName + " " +  stderr.decode("utf-8"))
+        return packageName, json.loads(stdout)
+
+    async def authenticateHash(self, hash, packageName):
+        cmd = [self.casPath, "authenticate", "--hash", hash, "--output=json"]
+        if(self.apiKey):
+            cmd.append("--api-key"), 
+            cmd.append(self.apiKey)
+        elif(self.signerId):
+            cmd.append("--signerID"), 
+            cmd.append(self.signerId)
+        stdout, stderr = await self.executeMe(cmd)
+        if(stderr):
+            self.logger.error(packageName + " " +  stderr.decode("utf-8"))
+        if(stdout):
+            return packageName, json.loads(stdout)
+        else:
+            return packageName, None
+
+    async def authenticateFile(self, absolutePath, packageName):
+        cmd = [self.casPath, "authenticate", absolutePath, "--output=json"]
         if(self.apiKey):
             cmd.append("--api-key"), 
             cmd.append(self.apiKey)
@@ -63,4 +131,8 @@ class CASClient:
             args.append("-r")
             args.append(reqFile)
         args.extend(additionalPipArgs)
-        _main(args)
+        what = _main(args)
+        return what == 0
+
+    def getPipVersion(self):
+        return pipVersion
--- cas_pip/cli.py
+++ cas_pip/cli.py
@@ -7,7 +7,10 @@ import os
 import json
 import sys
 from .casclient.casclient import CASClient, NotarizedStatus
+
 notarizedReqFilename = "~NOTARIZED_REQ_FILE~"
+notarizedReqPipVersion = "~NOTARIZED_REQ_PIPVERSION~"
+
 logger = logging.getLogger("cas_pip_cli")
 
 
@@ -28,45 +31,54 @@ def asynchronous(f):
 def cli():
     pass
 
-
-
-
-@click.command()
+@cli.command(name="authorize", help = "Authorizes pip packages from provided requirements file")
 @click.option('--reqfile', default="requirements.txt", help='Requirements file name')
 @click.option('--taskchunk', default=3, help='Max authorization request per once')
 @click.option('--caspath', default="cas", help='Path to cas binary')
 @click.option('--pipnoquiet', default=True, is_flag = True, show_default = True, help='Disables output of pip')
 @click.option('--nocache', default=True, is_flag = True, show_default = True, help='Disables cache of pip')
 @click.option('--signerid', help='Signer ID')
-@click.option('--apikey', help='API Key')
-@click.option('--output', default="-", help='Specifies output file "-" for printing to stdout')
+@click.option('--api-key', help='API Key')
+@click.option('--output', default="-", help='Specifies output file. "-" for printing to stdout. NONE for printing nothing')
+@click.option('--noprogress',  default=False, is_flag = True, show_default = True, help='Shows progress bar of action')
+@click.option('--notarizepip', default=False, is_flag = True, show_default = True, help='Notarizing also pip version')
 @asynchronous
-async def authorize(reqfile, taskchunk, caspath, pipnoquiet, nocache, signerid, apikey, output):
-    if(apikey == None and signerid == None):
+async def authorize(reqfile, taskchunk, caspath, pipnoquiet, nocache, signerid, api_key, output, noprogress, notarizepip):
+    if(api_key == None and signerid == None):
         apikey = os.environ.get("CAS_API_KEY", None)
         signerid = os.environ.get("SIGNER_ID", None)
         if(apikey == None and signerid == None):
             logger.error("You must provide CAS_API_KEY or SIGNER_ID environment or --apikey argument or --signerid arugment to authorize")
             sys.exit(1)
     statusCodeToRet = 0
-    casClient = CASClient(signerid, caspath, apikey)
+    casClient = CASClient(signerid, caspath, api_key)
     with tempfile.TemporaryDirectory() as tmpdirname:
-        casClient.downloadPipFiles(tmpdirname, reqFile = reqfile, quiet = pipnoquiet, noCache=nocache)
+        pipStatus = casClient.downloadPipFiles(tmpdirname, reqFile = reqfile, quiet = pipnoquiet, noCache=nocache)
+        if(not pipStatus):
+            sys.exit(1)
         filesIncluded = dict()
         tasks = []
         for file in os.walk(tmpdirname):
             for package in file[2]:
                 filesIncluded[package] = os.path.join(file[0], package)
-                tasks.append(casClient.authenticateFile(package, filesIncluded[package]))
+                tasks.append(casClient.authenticateFile(filesIncluded[package], package))
         gathered = []
 
         chunked = chunks(tasks, taskchunk)
-        with click.progressbar(chunked, label = f"Authorization") as bar:
-            for chunk in bar:    
+        if(not noprogress):
+            with click.progressbar(chunked, label = f"Authorization") as bar:
+                for chunk in bar:    
+                    gatheredChunk = await asyncio.gather(*chunk)
+                    gathered.extend(gatheredChunk)
+        else:
+            for chunk in chunked:    
                 gatheredChunk = await asyncio.gather(*chunk)
                 gathered.extend(gatheredChunk)
+
         authorizedSbom = dict()
-        gathered.append(await casClient.authenticateFile(notarizedReqFilename, reqfile))
+        gathered.append(await casClient.authenticateFile(reqfile, notarizedReqFilename))
+        if(notarizepip):
+            gathered.append(await casClient.notarizeHash(casClient.getSha256(casClient.getPipVersion()), notarizedReqPipVersion))
         for item in gathered:
             packageName, loaded = item
             if(loaded):
@@ -81,30 +93,36 @@ async def authorize(reqfile, taskchunk, caspath, pipnoquiet, nocache, signerid, 
 
         if(output == "-"):
             print(json.dumps(authorizedSbom, indent=4), flush=True)
+        elif(output == "NONE"):
+            pass
         else:
             with open(output, "w") as toWrite:
                 toWrite.write(json.dumps(authorizedSbom, indent=4))
     sys.exit(statusCodeToRet)
 
 
-@click.command()
+@cli.command(name="notarize", help = "Notarizes pip packages from provided requirements file")
 @click.option('--reqfile', default="requirements.txt", help='Requirements file name')
 @click.option('--taskchunk', default=3, help='Max authorization request per once')
 @click.option('--caspath', default="cas", help='Path to cas binary')
 @click.option('--pipnoquiet', default=True, is_flag = True, show_default = True, help='Disables output of pip')
 @click.option('--nocache', default=True, is_flag = True, show_default = True, help='Disables cache of pip')
-@click.option('--apikey', default=None, help='API Key')
-@click.option('--output', default="-", help='Specifies output file "-" for printing to stdout')
+@click.option('--api-key', default=None, help='API Key')
+@click.option('--output', default="-", help='Specifies output file. "-" for printing to stdout. NONE for printing nothing')
+@click.option('--noprogress',  default=False, is_flag = True, show_default = True, help='Shows progress bar of action')
+@click.option('--notarizepip', default=False, is_flag = True, show_default = True, help='Notarizing also pip version')
 @asynchronous
-async def notarize(reqfile, taskchunk, caspath, pipnoquiet, nocache, apikey, output):
-    if(apikey == None):
-        apikey = os.environ.get("CAS_API_KEY", None)
-        if(apikey == None):
-            logger.error("You must provide CAS_API_KEY environment or --apikey argument")
+async def notarize(reqfile, taskchunk, caspath, pipnoquiet, nocache, api_key, output, noprogress, notarizepip):
+    if(api_key == None):
+        api_key = os.environ.get("CAS_API_KEY", None)
+        if(api_key == None):
+            logger.error("You must provide CAS_API_KEY environment or --api_key argument")
             sys.exit(1)
-    casClient = CASClient(None, caspath, apikey)
+    casClient = CASClient(None, caspath, api_key)
     with tempfile.TemporaryDirectory() as tmpdirname:
-        casClient.downloadPipFiles(tmpdirname, reqFile = reqfile, quiet=pipnoquiet, noCache=nocache)
+        pipStatus = casClient.downloadPipFiles(tmpdirname, reqFile = reqfile, quiet=pipnoquiet, noCache=nocache)
+        if(not pipStatus):
+            sys.exit(1)
         filesIncluded = dict()
         tasks = []
         for file in os.walk(tmpdirname):
@@ -114,10 +132,16 @@ async def notarize(reqfile, taskchunk, caspath, pipnoquiet, nocache, apikey, out
         
         gathered = []
         chunked = chunks(tasks, taskchunk)
-        with click.progressbar(chunked, label = f"Notarization") as bar:
-            for chunk in bar:    
+        if(not noprogress):
+            with click.progressbar(chunked, label = f"Notarization") as bar:
+                for chunk in bar:    
+                    gatheredChunk = await asyncio.gather(*chunk)
+                    gathered.extend(gatheredChunk)
+        else:
+            for chunk in chunked:    
                 gatheredChunk = await asyncio.gather(*chunk)
                 gathered.extend(gatheredChunk)
+
         sbom = dict()
         for item in gathered:
             try:
@@ -129,8 +153,14 @@ async def notarize(reqfile, taskchunk, caspath, pipnoquiet, nocache, apikey, out
                 sys.exit(1)
         name, notarization = await casClient.notarizeFile(reqfile, notarizedReqFilename)
         sbom[name] = notarization
+        if(notarizepip):
+            name, notarization = await casClient.notarizeHash(casClient.getSha256(casClient.getPipVersion()), notarizedReqPipVersion)
+            sbom[name] = notarization
+
         if(output == "-"):
             print(json.dumps(sbom, indent=4), flush=True)
+        elif(output == "NONE"):
+            pass
         else:
             with open(output, "w") as toWrite:
                 toWrite.write(json.dumps(sbom, indent=4))
